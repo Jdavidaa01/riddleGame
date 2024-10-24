f9e2c733b35906ffc9449f9a4b23c35456005a22
Jdavidaa01 <jdavidaa.01@gmail.com>
2024-10-24 11:50:27 -0500

    changeBKsoundindex

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 let numberRandom = Math.floor(Math.random()*100)+1;
-let getname =document.getElementById('nameplayer');
-let getnum =document.getElementById('numplayer');
+let nameplayer =document.getElementById('nameplayer');
+let numplayer =document.getElementById('numplayer');
 let mesage =document.getElementById('mesage');
-let fondo = document.getElementById('background');
-let sound = new Audio('/mp3/aud.mp3');
-document.body.style.backgroundImage('/img/bkg2.png');
+let background = document.getElementById('background');
+let sound = new Audio('/mp3/bites-ta-da-winner.mp3');
+
 function checkAns(){
-    let name =getname.value;
+    let name=nameplayer.value;
         
-    let numIn= parseInt(getnum.value);
-    console.log(getname);
+    let numIn= parseInt(numplayer.value);
+    console.log(numberRandom);
     if(isNaN(numIn)||numIn<1 || numIn>100){
         mesage.textContent='Por favor, introduce un numero valido entre 1 y 100. ';
         return;
     }
     if(numIn === numberRandom){
         mesage.textContent=  name.toUpperCase()+' Felicidades! Has adivinado el numero correcto. !';
-        mesage.style.color='green';
+        mesage.style.color='white';
         mesage.style.fontSize='4rem';
         sound.play();
-        document.body.style.backgroundImage('/img/bkg.png');
-        getnum.disabled=true;
+        document.body.style.backgroundImage=("url(/img/img2.jpg)");
+        numplayer.disabled=true;
     }else if (numIn<numberRandom){
         sound.pause();
         mesage.textContent='El numero es mayor. Intenta nuevamente';
